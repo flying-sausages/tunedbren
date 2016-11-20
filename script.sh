df56d5b7ac2d9eb18948ee4ea1563514bf91ef6e
ae-g-i-s <github@xn--g-3fa.is>
2016-11-20 21:46:53 +0100

    Args parsing, skeleton for main loop

--- script.sh
+++ script.sh
@@ -33,6 +33,8 @@ OPTIONS:
 		Set the Artist field that will be used for dir-template.
 	-ta 'artist'
 		Set the Artist field that will be used for file-template.
+	-l 'album'
+		Set the Album field.
 	-c 'cataloguenumber'
 		Set the Catalogue Number field.
 	-y 'year'
@@ -56,23 +58,79 @@ TEMPLATES
 		%src - Source
 		
 EOF
-	exit -1
 }
 
-[[ $# -lt 1 ]] && usage
+# If there are no args...show usage
+[[ $# -lt 1 ]] && { usage; exit -1; }
 
-dir=$1
 
+IN_DIR=''
+DIR_TEMPLATE='%a - %l (%src %b) [%c %y]'
+DIR_ARTIST=''
+FILE_TEMPLATE='0%n. %a - %s'
+FILE_ARTIST=''
+ALBUM=''
+CATALOG=''
+YEAR=''
+BITRATE=''
+SOURCE=''
 
-id3convert "$dir"/*.mp3 || {
-	echo " failed"
+# Gather command line arguments by shifting them off one by one
+while [ $# -gt 0 ]
+do
+	case "$1" in
+		-t) shift; FILE_TEMPLATE="$1";
+			;;
+		-d) shift; DIR_TEMPLATE="$1";
+			;;
+		-a) shift; DIR_ARTIST="$1";
+			;;
+		-ta) shift; FILE_ARTIST="$1";
+			;;
+		-l) shift; ALBUM="$1";
+			;;
+		-c) shift; CATALOG="$1";
+			;;
+		-y) shift; YEAR="$1";
+			;;
+		-b) shift; BITRATE="$1";
+			;;
+		-src) shift; SOURCE="$1";
+			;;
+		*) IN_DIR="$1";
+			;;
+	esac
+	shift;
+done
+
+# A few sanity checks...
+[ -z "$IN_DIR" ] && { echo "${name}: No input directory."; usage; exit -2 }
+[ -d "$IN_DIR" ] || { echo "${name}: '$IN_DIR' is not a directory."; exit -3 }
+[ -z "$FILE_TEMPLATE" ] && { echo "${name}: No file template."; exit -3 }
+[ -z "$DIR_TEMPLATE" ] && { echo "${name}: No directory template."; exit -3 }
+
+# Convert ID3 tags if there are any MP3 files
+[ $(ls "$IN_DIR" | grep -q '\.mp3$') ] && { 
+	id3convert "$IN_DIR"/*.mp3 || {
+		echo "${name}: Failed to convert ID3 tags, continuing anyway."
+	}
 }
 
-#Should ask what standard to use for renaming
-    #Or make one quickly on the fly, and allow to save it and reuse
+for filetype in filetypes
+do
+	ls "$IN_DIR"/*."$filetype" | while read infile
+	do
+		case filetype in
+			mp3) 
+				;;
+			flac)
+				;;
+			*) echo "${name}: Unimplemented filetype $filetype"
+				;;
+		esac
+	done
+done
 
-# make sure to convert all the tags first
-id3convert "$dir"/.mp3
 
 #echo "Wanna check and possibly edit the tags before we move all this shit? [N/y]"
 # If yes, read out song nr, track, artist, album and year into stdout
@@ -107,4 +165,4 @@ sourceCaps=${source^^}}
 newdir="${dir}../${artist} - ${album} (${source} ${bitrate}) [${catnr} $year]]"
 
 echo "Moving to new dir = ${newdir}"
-mv "$dir" "${newdir}"
+mv "$IN_DIR" "${newdir}"
